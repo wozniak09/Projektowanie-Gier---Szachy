fdb124849bb2497888c814be81e76f51fa059ef1
Daniel Woźniak <daniel.wozniak@wp.eu>
2016-03-16 00:32:25 +0100

    Utorzenie projektu. Dodanie Szachownicy ver.1

---
+++ Program/src/projektowanie/gier/szachy/ProjektowanieGierSzachy.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projektowanie.gier.szachy;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ProjektowanieGierSzachy extends JPanel {
+    
+    
+    public void paint(Graphics g) {
+        g.fillRect(100, 100, 400, 400);
+        for(int i = 100; i <= 400; i+=100) {
+            for(int j = 100; j <= 400; j+=100) {
+                g.clearRect(i, j, 50, 50);
+            }
+        }
+        
+        for(int i = 150; i <= 450; i+=100) {
+            for(int j = 150; j <= 450; j+= 100) {
+                g.clearRect(i, j, 50, 50);
+            }
+        }
+    }
+
+ 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600,600);
+        frame.getContentPane().add(new ProjektowanieGierSzachy());
+        frame.setLocationRelativeTo(null);
+        frame.setBackground(Color.LIGHT_GRAY);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+}
